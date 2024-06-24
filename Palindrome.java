3dda2700fd7aafb35fd6ad47825527c815007f51
Santhoshjk159 <jksanthosh159@gmail.com>
2024-06-24 16:28:47 +0530

    first time dude

---
+++ Palindrome.java
@@ -0,0 +1,15 @@
+public class Palindrome {
+
+    public static void main(String[] args) {
+        String str = "SACHIN TENDULKAR";
+        StringBuilder rev = new StringBuilder(str).reverse();
+        
+        if (str.equals(rev.toString())) {
+            System.out.println("yes");
+        } else {
+            System.out.println("No");
+        }
+        System.out.println("GIT");
+
+    }
+}
